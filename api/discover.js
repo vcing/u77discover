3d57f197c4c3adc9e1450492ed36eadfce16dff1
Vcing <2131287@qq.com>
2015-12-22 18:54:11 +0800

    update discover detail page

--- api/discover.js
+++ api/discover.js
@@ -14,6 +14,33 @@ router.post('/',function(req,res){
 	})
 });
 
+router.get('/list',function(req,res){
+	var Discover = AV.Object.extend('Discover');
+	var query    = new AV.Query(Discover);
+	query.descending('createdAt');
+	if(req.params.page){
+		query.skip((req.params.page-1) * 20);
+	}
+	query.limit(20);
+	query.find().then(function(result){
+		res.json(result);
+	},function(err){
+		res.json(err);
+	});
+});
+
+router.get('/:id',function(req,res){
+	var Discover = AV.Object.extend('Discover');
+	var query    = new AV.Query(Discover);
+	query.equalTo('discoverId',parseInt(req.params.id));
+	query.include('game');
+	query.first().then(function(discover){
+		res.json(discover);
+	},function(err){
+		res.json(err);
+	});
+});
+
 function validDiscover(discover){
 	var result         = {};
 	result.oneWord     = discover.oneWord;
@@ -24,7 +51,7 @@ function validDiscover(discover){
 	result.cover       = discover.cover;
 	result.title       = discover.title;
 	var Discover       = AV.Object.extend('Discover');
-	var _discover       = new Discover(result);
+	var _discover      = new Discover(result);
 	_discover.set('game',AV.Object.createWithoutData("Game", discover.game));
 	return _discover.save();
 }
