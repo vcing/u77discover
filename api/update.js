6fb66195a9c6699f9b93fc289b5921b9ea0b5b3c
dc540127070 <540127070@qq.com>
2016-08-01 11:16:59 +0800

    fix img

--- api/update.js
+++ api/update.js
@@ -7,6 +7,76 @@ var crypto  = require('crypto');
 var signKey = require('../config/config.js').signKey;
 
 
+router.get("/fiximg",function(req,res){
+	getimg().then(function(result){
+		changeImg(result);
+		res.send("ok");
+	});
+});
+
+function getimg(deffered,arr=[],discoverId){
+	if(!deffered){
+		deffered = q.defer();
+	}
+	var Discover = global.Discover;
+	var query = new AV.Query(Discover);
+	query.ascending('discoverId');
+	query.include('game')
+	query.limit(1000);
+	if(discoverId){
+		query.greaterThan("discoverId",parseInt(discoverId));
+	}
+	query.find().then(function(result){
+		arr = arr.concat(result);
+		console.log(arr.length);
+		if(result.length == 1000){
+			getimg(deffered,arr,result[result.length-1].get("discoverId"));
+		}else{
+
+			return deffered.resolve(arr);
+		}
+	});
+	return deffered.promise;
+}
+
+function changeImg(data){
+	console.log(data.length);
+	for(var i=0;i<data.length;i++){
+		var discover = data[i];
+		var game = discover.get("game");
+		var imgs = game.get("img");
+		for(var x=0;x<imgs.length;x++){
+			imgs[x].url = imgs[x].url.replace("img.u77.com","u77img.b0.upaiyun.com");
+		}
+		game.set("img",imgs);
+		game.save();
+
+
+		var cover = discover.get("cover");
+		var avatar = discover.get("avatar");
+		var discoverimgs = discover.get("img");
+		for(var y=0;y<discoverimgs.length;y++){
+			discoverimgs[y] = discoverimgs[y].replace("img.u77.com","u77img.b0.upaiyun.com");
+		}
+		cover = cover.replace("img.u77.com","u77img.b0.upaiyun.com");
+		avatar = avatar.replace("img.u77.com","u77img.b0.upaiyun.com");
+		discover.set("cover",cover);
+		discover.set("avatar",avatar);
+		discover.set("img",discoverimgs);
+		
+		discover.save();
+
+		console.log(i);
+	}
+
+	return "ok";
+}
+
+router.get('/test',function(req,res){
+	console.log("asjkfhajkfkllj");
+	res.send("asfaf");
+});
+
 router.post('/delete',function(req,res){
 	var discoverId = req.body.discoverId;
 	var userId = req.body.userId;
@@ -136,5 +206,7 @@ router.post('/',function(req,res){
 		res.json(err);
 	}
 });
+
+
 
 module.exports = router;
